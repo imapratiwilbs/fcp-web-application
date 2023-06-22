e09e78c4dde60b2225f296f1e4fce338a46762e7
imapratiwilbs <ima.pratiwilbs@gmail.com>
2023-06-22 21:36:52 +0700

    y

--- client/cookie.go
+++ client/cookie.go
@@ -18,8 +18,12 @@ func GetClientWithCookie(token string, cookies ...*http.Cookie) (*http.Client, e
 	})
 
 	jar.SetCookies(&url.URL{
-		Scheme: "http",
-		Host:   "localhost:8080",
+		// Scheme: "http",
+		// Host:   "localhost:8080",
+
+		
+		Scheme: "https",
+		Host:   "fcp-web-application-production-0afb.up.railway.app",
 	}, cookies)
 
 	c := &http.Client{
